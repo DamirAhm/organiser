508a4d51414b3c68927d63509a4c2c91dbe88761
damirahm <damir221003@gmail.com>
2021-10-09 12:57:30 +0300

    Added route to delete items

--- server/routes/itemsRouter.ts
+++ server/routes/itemsRouter.ts
@@ -2,6 +2,7 @@ import { FastifyPluginCallback, RawServerDefault } from "fastify";
 import { Item } from '../types';
 import { RESPONSE_TYPES, STATUS_CODES } from '../constants';
 import ItemModel from '../database/models/item';
+import { ObjectId } from 'mongoose';
 
 // ? Path: /items
 const itemsRouter: FastifyPluginCallback<any, RawServerDefault> = (router, opts, done) => {
@@ -27,12 +28,47 @@ const itemsRouter: FastifyPluginCallback<any, RawServerDefault> = (router, opts,
 				return { error: "Parent property must be string or ObjectId" };
 			}
 
-			parentItem.subItems.push(newItem.id);
+			parentItem.subItems.push( newItem.id );
 			await parentItem.save();
 
-			return { payload: newItem }
 		}
+		return { payload: newItem }
 	} )
+
+	router.delete( '/', async (req, res) => {
+		const { id: itemId } = req.body as { id: unknown };
+
+		if ( !itemId || typeof itemId !== 'string' ) {
+			res.status( STATUS_CODES.BAD );
+			res.type( RESPONSE_TYPES.JSON );
+			return { error: "You must pass id with type string" }
+		}
+
+		const itemToDelete = await ItemModel.findByIdAndDelete( itemId );
+
+
+		if ( itemToDelete === null ) return {};
+		const deleted: ObjectId[] = [ itemToDelete.id ];
+
+		if ( itemToDelete.parent !== null ) {
+			const parentItem = await ItemModel.findById( itemToDelete.parent );
+
+			if ( parentItem ) {
+				parentItem.subItems = parentItem.subItems.filter( id => id.toString() !== itemId );
+				await parentItem.save();
+			}
+		}
+		if ( itemToDelete.subItems.length !== 0 ) {
+			for ( const subItemId of itemToDelete.subItems ) {
+				await ItemModel.deleteOne( { _id: subItemId } );
+				deleted.push( subItemId );
+			}
+		}
+
+
+		return { payload: deleted };
+	} )
+
 
 	done();
 }
