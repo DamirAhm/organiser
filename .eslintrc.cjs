bbb9776652c482fd7c46153d0157bbcbcb707a5b
damirahm <damir221003@gmail.com>
2022-02-25 23:31:03 +0300

    Added eslint to project and removed accidentally added deps from package.json

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'linux'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+	},
+};
